0a9837a35129e8bcffe909688fcf5e7c6a29ceb8
Kevin Spiekermann <kspieker@mit.edu>
2020-12-13 20:07:48 -0500

    update suball submit script

--- acs/scripts/suball_conf_screen.sh
+++ acs/scripts/suball_conf_screen.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+ACS_PATH="/home/gridsan/kspieker/ENI/ACS"
+
 while (( "$#" )); do
-	 sbatch -J $1 submit_conf_screen.sh $1
+	 sbatch -W -J $1 "${ACS_PATH}/acs/scripts/submit_conf_screen.sh" $1
 	 shift
  done
+wait
