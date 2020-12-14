5781f1c59d41cd95f86c5b7ca4d3ed42faa1be39
Kevin Spiekermann <kspieker@mit.edu>
2020-12-13 20:12:58 -0500

    update run_ACS
    Let the bash script wait until the current step is done running before moving onto the next step. Also time each section

--- acs/scripts/run_ACS.sh
+++ acs/scripts/run_ACS.sh
@@ -29,62 +29,79 @@ step10="${ACS_PATH}acs/analysis/calc_mstst_rate.py"
 # step 1: generate conformers
 echo "Step 1: Generating conformers..."
 source activate arc_env
+START_TIME=$SECONDS
 python $step1 input.yml
-echo "Completed step 1"
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 1 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 2: run sp calculations with psi4
 echo "Step 2: Running single point calculations with Psi4..."
 cd initial_sp_screening
+START_TIME=$SECONDS
 bash $step2 */*.yml
-echo "Completed step 2"
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 2 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 3: analyze sp screening results and find set of lowest energy conformers
 # cd into one of the directories, such as 0, which will always be present
 echo "Step 3: Analyzing Psi4 single point screening"
 cd 0
+START_TIME=$SECONDS
 python $step3 initial_conf_screening_result.yml
-echo "Completed step 3"
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 3 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 4: run geometry optimizations for the lower energy conformers
 echo "Step 4: Running QM jobs..."
 cd ../../opt
-sbatch $step4
-echo "Completed step 4"
+START_TIME=$SECONDS
+sbatch -W $step4
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 4 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 5: analyze geometry optimizations and frequency calculations
 echo "Step 5: Analyzing results from QM job..."
+START_TIME=$SECONDS
 python $step5 opt_project_info.yml
-echo "Completed step 5"
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 5 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 6: fine tune the optimization at higher level of theory
 echo "Step 6: Running QM jobs for fine optimization..."
 cd ../opt_fine
-sbatch $step6
-echo "Completed step 6"
-
+START_TIME=$SECONDS
+sbatch -W $step6
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 6 in $ELAPSED_TIME seconds" $'\n'
 
 # step 7: analzye the fine optimization
 echo "Step 7: Analyzing results from fine QM job..."
+START_TIME=$SECONDS
 python $step7 fine_opt_project_info.yml
-echo "Completed step 7"
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 7 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 8: run single point calculations
 echo "Step 8: Submitting single point job..."
 cd ../sp
-bash $step8
-echo "Completed step 8"
+START_TIME=$SECONDS
+sbatch -W $step8
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 8 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 9: analyze the single point calculations
 echo "Step 9: Analyzing single point job..."
+START_TIME=$SECONDS
 python $step9 sp_project_info.yml
-echo "Completed step 9"
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed step 9 in $ELAPSED_TIME seconds" $'\n'
 
 
 # step 10: run MSTST calculation with final set of optimized conformers
